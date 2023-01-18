9d339300ca5ec2ade19d80df7417e932e64a0332
TXTSpake <108833714+Kakita5595@users.noreply.github.com>
2023-01-18 13:26:12 +0100

    deployment

---
+++ models/Paciente.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const pacientesSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        requiered: true,
+    },
+    propietario: {
+        type: String,
+        requiered: true,
+    },
+    email: {
+        type: String,
+        requiered: true,
+    },
+    fecha: {
+        type: Date,
+        requiered: true,
+        default: Date.now(),
+    },
+    sintomas: {
+        type: String,
+        requiered: true,
+    },
+    veterinario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Veterinario',
+    },
+}, 
+    {
+        timestamps: true,
+    }
+);
+
+const Paciente = mongoose.model('Paciente', pacientesSchema);
+
+export default Paciente;
